127613b30802275add7e2f78310b20c2bfd44d7d
Ruhannn <kamiayaka09@gmail.com>
2024-05-21 10:23:32 +0600

    product schema + eslint

---
+++ src/models/Product.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface Variant {
+  type: string;
+  value: string;
+}
+
+interface Inventory {
+  quantity: number;
+  inStock: boolean;
+}
+
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: Variant[];
+  inventory: Inventory;
+}
+
+const VariantSchema: Schema = new Schema(
+  {
+    type: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const InventorySchema: Schema = new Schema(
+  {
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
+  },
+  { _id: false }
+);
+
+const ProductSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [VariantSchema], required: true },
+  inventory: { type: InventorySchema, required: true },
+});
+
+export default mongoose.model<IProduct>("Product", ProductSchema);
